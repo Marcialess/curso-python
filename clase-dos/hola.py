0cc7a5a6aedc8d7c7f530dea5c72ae49bbba1e78
Marcial Abad <108768099+Marcialess@users.noreply.github.com>
2023-04-19 22:37:07 -0400

    agregando carpetas

---
+++ clase-dos/hola.py
@@ -0,0 +1,19 @@
+print("Hola Marcial");
+
+edad = 34;
+sueldo = 5000.34;
+
+nombre = "Marcial";
+Apellido = "Abad";
+Student = True;
+
+print(edad, sueldo, nombre, Apellido, Student);
+
+numero_1 = 1;
+numero_2 = 2;
+numero_3 = 28;
+
+
+TestList = [numero_1 + numero_2 + numero_3];
+print (TestList);
+
